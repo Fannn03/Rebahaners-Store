903808b2aa1af5bba48b9e4922106e06d82d9126
Fannn03 <fann.fann771@gmail.com>
2021-12-13 18:48:26 +0700

    add table orders

---
+++ database/migrations/2021_12_13_114330_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer("id_transaction")->unique();
+            $table->string('product_id');
+            $table->string('name_product');
+            $table->string('username');
+            $table->integer("price");
+            $table->enum('status', ['0', '1']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
