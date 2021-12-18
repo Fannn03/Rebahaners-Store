27ed1139d2558ec3803aa8273b61bc352bb090c4
Fannn03 <fann.fann771@gmail.com>
2021-12-18 11:36:44 +0700

    remove if conditions

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,6 @@
             <img src="https://itemku.com/_next/image?url=https%3A%2F%2Fd1x91p7vw3vuq8.cloudfront.net%2Fplaceholder%2F2021121%2Fbjhmyerx73jgys4z9ljhu8.png&w=1920&q=75" class="rounded-lg bg-cover w-full" alt="">
         </div>
         <hr class="my-5">
-        @if (!$category)
         <div class="grid grid-cols-3 gap-4 px-3">
             @foreach ($category as $ct)
             <a href="{{ url('/category/' . $ct->slug) }}" class="flex flex-col space-y-3 h-28 bg-local transform transition duration-300 hover:scale-110 rounded-md p-3 bg-cover" style="background-image: linear-gradient(rgba(22, 20, 21, 0.8), rgba(22, 20, 21, 0.8)), url({{ asset('storage/images/category/'. $ct->photo) }})">
@@ -15,11 +14,6 @@
             </a>
             @endforeach
         </div>
-        @else
-        <div class="text-center text-gray-500">
-            <p>Category kosong atau belum dibuat oleh admin.</p>
-        </div>
-        @endif
     </div>
 </div>
 @endsection
